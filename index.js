c64bb2e56751089292a553317e38632d8dcbec70
Jarek Lipski <loomchild@loomchild.net>
2021-09-12 20:25:35 +0200

    dump config file

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function init () {
   }
 
   const configFile = await readFile('webflowgit.yml')
+  console.log(JSON.stringify(YAML.parse(configFile), null, 2))
   Object.apply(config, YAML.parse(configFile))
 
   return config
