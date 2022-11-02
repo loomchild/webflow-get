8736d1d2f554873213536ab02d9718ec8176950a
Viktor <thetravlrd@gmail.com>
2022-11-02 01:59:17 +0100

    adding main function

--- src/index.js
+++ src/index.js
@@ -28,4 +28,15 @@ const getWebflowPublishDate = pipe([
     extractWebflowPublishDate,
 ]);
 
+async function main() {
+    await updateSnapshot();
+}
 
+main()
+    .then(() => {
+        console.log('Executed successfully')
+    })
+    .catch((error) => {
+        console.error(error)
+        core.setFailed(error.message)
+    })
